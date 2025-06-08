0d02ca58f378b283bd8727079ddc2aeba9bb2bd9
mlukenich <73347955+mlukenich@users.noreply.github.com>
2025-06-07 20:22:10 -0400

    Update Parser.java

--- Project1_Skeleton/Parser.java
+++ Project1_Skeleton/Parser.java
@@ -1,14 +1,14 @@
-// CMSC 330 Advanced Programming Languages
-// Project 1 Skeleton
-// UMGC CITE
-// August 2021
+// Name: [Your Name]
+// Date: [Current Date]
+// Project: CMSC 330 Project 1
+// Description: Modified to parse the expanded grammar for new shapes.
 
 import java.awt.*;
 import java.io.*;
 import java.util.*;
 import javax.swing.*;
 
-// This class provides the skeleton parser for project 1
+// This class provides the parser for project 1
 
 class Parser {
     private JFrame window;
@@ -36,20 +36,22 @@ class Parser {
     }
 
     // Parses the following productions
-
     // images -> image images | image
-    // image -> right_triangle | rectangle
-    // right_triangle -> RIGHT_TRIANGLE COLOR number_list AT number_list HEIGHT NUMBER WIDTH NUMBER ';'
-    // rectangle -> RECTANGLE_ COLOR number_list AT number_list HEIGHT NUMBER WIDTH NUMBER ';'
+    // MODIFICATION START
+    // Updated to include all new shapes
+    // image -> right_triangle | rectangle | isosceles | parallelogram | regular_polygon | text
+    // ... plus new grammar rules
+    // MODIFICATION END
 
     private void parseImages(Scene scene, Token imageToken) throws LexicalError, SyntaxError, IOException {
-        int height, width, offset, radius;
+        int height, width, offset, radius, sides;
         verifyNextToken(Token.COLOR);
         int[] colors = getNumberList(3);
         Color color = new Color(colors[0], colors[1], colors[2]);
         verifyNextToken(Token.AT);
         int[] location = getNumberList(2);
         Point point = new Point(location[0], location[1]);
+
         if (imageToken == Token.RIGHT_TRIANGLE) {
             verifyNextToken(Token.HEIGHT);
             verifyNextToken(Token.NUMBER);
@@ -68,6 +70,40 @@ class Parser {
             width = lexer.getNumber();
             Rectangle rectangle = new Rectangle(color, point, height, width);
             scene.addImage(rectangle);
+        // MODIFICATION START - Add parsing for new shapes
+        } else if (imageToken == Token.ISOSCELES) {
+            verifyNextToken(Token.HEIGHT);
+            verifyNextToken(Token.NUMBER);
+            height = lexer.getNumber();
+            verifyNextToken(Token.WIDTH);
+            verifyNextToken(Token.NUMBER);
+            width = lexer.getNumber();
+            IsoscelesTriangle triangle = new IsoscelesTriangle(color, point, height, width);
+            scene.addImage(triangle);
+        } else if (imageToken == Token.PARALLELOGRAM) {
+            // NOTE: The grammar is "AT point1 point2 OFFSET number", so we parse another point list.
+            int[] point2_coords = getNumberList(2);
+            Point point2 = new Point(point2_coords[0], point2_coords[1]);
+            verifyNextToken(Token.OFFSET);
+            verifyNextToken(Token.NUMBER);
+            offset = lexer.getNumber();
+            Parallelogram parallelogram = new Parallelogram(color, point, point2, offset);
+            scene.addImage(parallelogram);
+        } else if (imageToken == Token.REGULAR_POLYGON) {
+            verifyNextToken(Token.SIDES);
+            verifyNextToken(Token.NUMBER);
+            sides = lexer.getNumber();
+            verifyNextToken(Token.RADIUS);
+            verifyNextToken(Token.NUMBER);
+            radius = lexer.getNumber();
+            RegularPolygon polygon = new RegularPolygon(color, sides, point, radius);
+            scene.addImage(polygon);
+        } else if (imageToken == Token.TEXT) {
+            verifyNextToken(Token.STRING);
+            String textContent = lexer.getLexeme();
+            Text text = new Text(color, point, textContent);
+            scene.addImage(text);
+        // MODIFICATION END
         } else {
              throw new SyntaxError(lexer.getLineNo(), "Unexpected image name " + imageToken);
         }
