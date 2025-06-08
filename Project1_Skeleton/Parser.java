bf2c25985c6fc5a2ad680c1b937b511e7ee9455f
mlukenich <73347955+mlukenich@users.noreply.github.com>
2025-06-08 13:56:27 -0400

    Update Parser.java

--- Project1_Skeleton/Parser.java
+++ Project1_Skeleton/Parser.java
@@ -1,7 +1,7 @@
 // Name: [Your Name]
-// Date: [Current Date]
+// Date: June 8, 2025
 // Project: CMSC 330 Project 1
-// Description: Modified to parse the expanded grammar for new shapes.
+// Description: Modified to parse the expanded grammar for all new shapes.
 
 import java.awt.*;
 import java.io.*;
@@ -9,21 +9,17 @@ import java.util.*;
 import javax.swing.*;
 
 // This class provides the parser for project 1
-
 class Parser {
-    private JFrame window;
     private Token token;
     private Lexer lexer;
 
     // Constructor to create new lexical analyzer from input file
-
     public Parser(File file) throws IOException {
         lexer = new Lexer(file);
     }
 
     // Parses the production
     // scene -> SCENE IDENTIFIER number_list images END '.'
-
     public Scene parseScene() throws LexicalError, SyntaxError, IOException {
         verifyNextToken(Token.SCENE);
         verifyNextToken(Token.IDENTIFIER);
@@ -35,14 +31,7 @@ class Parser {
         return scene;
     }
 
-    // Parses the following productions
-    // images -> image images | image
-    // MODIFICATION START
-    // Updated to include all new shapes
-    // image -> right_triangle | rectangle | isosceles | parallelogram | regular_polygon | text
-    // ... plus new grammar rules
-    // MODIFICATION END
-
+    // Parses all image productions recursively
     private void parseImages(Scene scene, Token imageToken) throws LexicalError, SyntaxError, IOException {
         int height, width, offset, radius, sides;
         verifyNextToken(Token.COLOR);
@@ -70,7 +59,6 @@ class Parser {
             width = lexer.getNumber();
             Rectangle rectangle = new Rectangle(color, point, height, width);
             scene.addImage(rectangle);
-        // MODIFICATION START - Add parsing for new shapes
         } else if (imageToken == Token.ISOSCELES) {
             verifyNextToken(Token.HEIGHT);
             verifyNextToken(Token.NUMBER);
@@ -81,7 +69,6 @@ class Parser {
             IsoscelesTriangle triangle = new IsoscelesTriangle(color, point, height, width);
             scene.addImage(triangle);
         } else if (imageToken == Token.PARALLELOGRAM) {
-            // NOTE: The grammar is "AT point1 point2 OFFSET number", so we parse another point list.
             int[] point2_coords = getNumberList(2);
             Point point2 = new Point(point2_coords[0], point2_coords[1]);
             verifyNextToken(Token.OFFSET);
@@ -103,7 +90,6 @@ class Parser {
             String textContent = lexer.getLexeme();
             Text text = new Text(color, point, textContent);
             scene.addImage(text);
-        // MODIFICATION END
         } else {
              throw new SyntaxError(lexer.getLineNo(), "Unexpected image name " + imageToken);
         }
@@ -113,14 +99,7 @@ class Parser {
             parseImages(scene, token);
     }
 
-    // Parses the following productions
-
-    // number_list -> '(' numbers ')'
-    // numbers -> NUMBER | NUMBER ',' numbers
-
-    // Returns an array of the numbers in the number list
-
-    private int[]  getNumberList(int count) throws LexicalError, SyntaxError, IOException {
+    private int[] getNumberList(int count) throws LexicalError, SyntaxError, IOException {
         int[] list = new int[count];
         verifyNextToken(Token.LEFT_PAREN);
         for (int i = 0; i < count; i++) {
@@ -135,35 +114,6 @@ class Parser {
         return list;
     }
 
-    // Returns a list of numbers
-
-    private int[] getNumberList() throws LexicalError, SyntaxError, IOException {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        verifyNextToken(Token.LEFT_PAREN);
-        do {
-            verifyNextToken(Token.NUMBER);
-            list.add((int) lexer.getNumber());
-            token = lexer.getNextToken();
-        }
-        while (token == Token.COMMA);
-        verifyCurrentToken(Token.RIGHT_PAREN);
-        int[] values = new int[list.size()];
-        for (int i = 0; i < values.length; i++)
-            values[i] = list.get(i);
-        return values;
-    }
-
-    // Verifies that the next token is the expected token
-
     private void verifyNextToken(Token expectedToken) throws LexicalError, SyntaxError, IOException {
         token = lexer.getNextToken();
-        verifyCurrentToken(expectedToken);
-    }
-
-    // Verifies that the current token is the expected token
-
-    private void verifyCurrentToken(Token expectedToken) throws SyntaxError {
-        if (token != expectedToken)
-            throw new SyntaxError(lexer.getLineNo(), "Expecting token " + expectedToken + " not " + token);
-    }
-}
+        verifyCurrentToken(expected
